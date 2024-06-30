59ccddf0f9182585c7379f825fa7d84a33be214f
aayushh14 <121049803+aayushh14@users.noreply.github.com>
2024-07-01 00:31:13 +0530

    Update BurnAndMint.sol

--- BurnAndMint.sol
+++ BurnAndMint.sol
@@ -1,1 +1,21 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+        // Optionally initialize state variables or perform other setup
+    }
+
+    function mint(address to, uint256 amount) public returns (bool) {
+        _mint(to, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) public returns (bool) {
+        _burn(msg.sender, amount);
+        return true;
+    }
+}
